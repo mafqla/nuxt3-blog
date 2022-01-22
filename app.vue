65efb9e41cc8b050ac77491b248f9809de3e0821
mafqla <1692285748@qq.com>
2022-01-22 20:26:55 +0800

    初始化

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>hello word</h1>
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>index page</div>
+  <NuxtLink to="/detail">Detail Page</NuxtLink>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
